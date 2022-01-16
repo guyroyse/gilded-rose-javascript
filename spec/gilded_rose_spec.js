7e55a691db8f7a26475e909cfd670f1e39826f3c
Sadat <saadatt@gmail.com>
2022-01-15 19:15:42 -0600

    added mocha syntax verification test to the test suite

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,14 @@
+const { expect } = require('chai');
+
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
-    update_quality();
-  });
+  // it("should do something", function() {
+  //   update_quality();
+  // });
+
+  it('should verify mocha works fine', () => {
+    let a = 1, b = 2;
+    expect(a).to.be.lessThan(b)
+  })
 
 });
