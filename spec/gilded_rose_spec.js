4a9d7f0d81f099a56402fae8d6c081438d4a58ab
Sadat <saadatt@gmail.com>
2022-01-15 23:22:47 -0600

    removed unnecessary commentary lines from spec file

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -13,15 +13,6 @@ describe('Gilded Rose', () => {
   //   update_quality();
   // });
 
-  // it('should verify mocha works fine', () => {
-  //   let a = 1, b = 2;
-  //   expect(a).to.be.lessThan(b)
-  // });
-  //
-  // it('should verify mocha test setup is a success', () => {
-  //   expect(true).to.be.true;
-  // });
-
   //do not alter the Item class or items property as those belong to the goblin in the corner
   it('should verify Item constructor is present', () => {
     expect(Item).to.be.a('function');
@@ -31,10 +22,9 @@ describe('Gilded Rose', () => {
     expect(items instanceof Array).to.be.true;
     expect(items.length).to.be.greaterThan(0);
   });
-  
+
   //We have a system in place that updates our inventory for us...
   it('should verify the presence of an update_quality function', () => {
     expect(update_quality).to.be.a('function');
   });
-
 });
