629d4be00a7fc17cca91c8c48f8595b5367906c0
Sadat <saadatt@gmail.com>
2022-01-17 12:57:59 -0600

    created a test to verify a class Item is present and fixed getItemData helper

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,20 @@
+const { expect } = require('chai');
+const { NAMES } = require('../src/constants');
+const {getItemData} = require('../src/helpers');
+const { Item } = require('../src/gilded_rose');
+
+
+
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
-    update_quality();
-  });
+  it('should verify new instance of Item class is succesfully created with matching data', () => {
+    const { name, sell_in, quality } = new Item(NAMES.AGED, ...getItemData(NAMES.AGED));
+
+    const [matching_sell_in, matching_quality] = getItemData(NAMES.AGED)
+
+    expect(name).to.be(NAMES.AGED);
+    expect(sell_in).to.be(matching_sell_in);
+    expect(quality).to.be(matching_quality);
+  })
 
 });
--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,7 @@
+const {ITEMS_DATA} = require('./constants');
+
 const getItemData = (key) => {
-    const { sell_in, quality } = itemsData[key];
+    const { sell_in, quality } = ITEMS_DATA[key];
 
     return [sell_in, quality];
 };
