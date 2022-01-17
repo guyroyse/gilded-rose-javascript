7c30dc3988223bef3f58ea13717290427efa7a50
Sadat <saadatt@gmail.com>
2022-01-17 13:31:24 -0600

    added a FAILING test to verify legacy update_quality f-n updates Sulfuras item the right way

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -7,6 +7,14 @@ const { Item } = require('../src/gilded_rose');
 
 describe("Gilded Rose", function() {
 
+  let items;
+
+  beforeEach(() => {
+    items = [
+      new Item(NAMES.SULFURAS, ...getItemData(NAMES.SULFURAS))
+    ];
+  });
+
   it('should verify new instance of Item class is succesfully created with matching data', () => {
     const { name, sell_in, quality } = new Item(NAMES.AGED, ...getItemData(NAMES.AGED));
 
@@ -17,4 +25,16 @@ describe("Gilded Rose", function() {
     expect(quality).to.equal(matching_quality);
   })
 
+  // The Quality of an item is never more than 50
+  it('should verify update_quality f-n correctly updates "Sulfuras"', () => {
+    update_quality(items);
+
+    const [{name, sell_in, quality}] = items;
+
+    const [matching_sell_in, matching_quality] = getItemData(NAMES.SULFURAS)
+
+    expect(name).to.equal(NAMES.SULFURAS);
+    expect(sell_in).to.equal(matching_sell_in);
+    expect(quality).to.equal(matching_quality);
+  });
 });
