984884703a22b9b264000d41721789338c574584
Sadat <saadatt@gmail.com>
2022-01-17 13:14:22 -0600

    made additional refactoring for items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,3 +1,6 @@
+const { NAMES } = require("../src/constants");
+const { getItemData } = require("../src/helpers");
+
 class Item {
   constructor(name, sell_in, quality) {
     this.name = name;
@@ -8,12 +11,12 @@ class Item {
 
 const items = []
 
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
+items.push(new Item(NAMES.DEX, ...getItemData(NAMES.DEX)));
+items.push(new Item(NAMES.AGED, ...getItemData(NAMES.AGED)));
+items.push(new Item(NAMES.ELIXIR, ...getItemData(NAMES.ELIXIR)));
+items.push(new Item(NAMES.SULFURAS, ...getItemData(NAMES.SULFURAS)));
+items.push(new Item(NAMES.BACKSTAGE, ...getItemData(NAMES.BACKSTAGE)));
+items.push(new Item(NAMES.CONJURED, ...getItemData(NAMES.CONJURED)));
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
